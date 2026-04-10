f3cf0cc4b5f7308633da29f21cce3bbfa522eb1d
NihatM <47819164+NihatM@users.noreply.github.com>
2026-04-10 08:41:38 +0400

    Fix N+1 query: add JOIN FETCH for markers/all endpoint

--- src/main/java/com/example/locationproject/repositories/MarkerRepository.java
+++ src/main/java/com/example/locationproject/repositories/MarkerRepository.java
@@ -9,19 +9,23 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MarkerRepository extends JpaRepository<Marker, Long> {
+    public interface MarkerRepository extends JpaRepository<Marker, Long> {
+
+    @Query("SELECT DISTINCT m FROM Marker m LEFT JOIN FETCH m.translations")
+            List<Marker> findAllWithTranslations();
+
     List<Marker> findByTitleIgnoreCase(String title);
 
 
     @Query("SELECT m FROM Marker m WHERE LOWER(m.title) LIKE LOWER(CONCAT('%', :title, '%'))")
-    List<Marker> findByTitleContainsIgnoreCase(@Param("title") String title);
+            List<Marker> findByTitleContainsIgnoreCase(@Param("title") String title);
 
 
 
 
     @Query("SELECT m FROM Marker m WHERE (m.title, m.description, m.markerType, m.latitude, m.longitude) IN " +
-            "(SELECT m2.title, m2.description, m2.markerType, m2.latitude, m2.longitude FROM Marker m2 " +
-            "GROUP BY m2.title, m2.description, m2.markerType, m2.latitude, m2.longitude HAVING COUNT(m2) > 1)")
-    List<Marker> findDuplicateMarkers();
+                       "(SELECT m2.title, m2.description, m2.markerType, m2.latitude, m2.longitude FROM Marker m2 " +
+                       "GROUP BY m2.title, m2.description, m2.markerType, m2.latitude, m2.longitude HAVING COUNT(m2) > 1)")
+            List<Marker> findDuplicateMarkers();
 
-}
+    }
